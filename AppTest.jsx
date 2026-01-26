9150c31d8b9bc8a6dcfc0d74524decb689bcd9e9
Paula McDade <paula@mac.mynetworksettings.com>
2026-01-25 20:02:32 -0500

    Test tabs + utils

--- AppTest.jsx
+++ AppTest.jsx
@@ -1,18 +1,30 @@
-// Testing all 4 tabs together
+// Testing all 4 tabs + utils
 import React from 'react';
 import { ChefHat } from 'lucide-react';
+import Papa from 'papaparse';
 import { useAppData } from './hooks/useAppData';
 import RecipesTab from './tabs/RecipesTab';
 import KDSTab from './tabs/KDSTab';
 import PrepTab from './tabs/PrepTab';
 import DeliveriesTab from './tabs/DeliveriesTab';
+import { getWeekId, getWeekIdFromDate } from './utils/weekUtils';
+import {
+  categorizeIngredient,
+  exportClientsCSV,
+  exportIngredientsCSV,
+  exportRecipesCSV,
+  parseClientsCSV,
+  parseIngredientsCSV,
+  parseRecipesCSV,
+  downloadCSV
+} from './utils';
 
 export default function App() {
   return (
     <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <ChefHat size={32} />
-      <h1 style={{ color: '#3d59ab' }}>All 4 Tabs Import Test!</h1>
-      <p>All tabs imported successfully</p>
+      <h1 style={{ color: '#3d59ab' }}>All Tabs + Utils Test!</h1>
+      <p>Week: {getWeekId()}</p>
     </div>
   );
 }
