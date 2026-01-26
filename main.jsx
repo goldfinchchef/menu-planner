494957f38caef7202539281f0889677f03c6eacd
Paula McDade <paula@mac.mynetworksettings.com>
2026-01-25 19:56:32 -0500

    Test real App.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './AppTest.jsx'
+import App from './App.jsx'
 import './index.css'
 
 // Placeholder pages for now
