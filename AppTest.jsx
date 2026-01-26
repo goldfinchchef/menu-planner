14f1ca3199a3293491bc8654a8a8ec5c88271abf
Paula McDade <paula@mac.mynetworksettings.com>
2026-01-25 20:00:17 -0500

    Test DeliveriesTab import

--- AppTest.jsx
+++ AppTest.jsx
@@ -1,16 +1,14 @@
-// Testing PrepTab import
+// Testing DeliveriesTab import
 import React from 'react';
 import { ChefHat } from 'lucide-react';
 import { useAppData } from './hooks/useAppData';
-import PrepTab from './tabs/PrepTab';
+import DeliveriesTab from './tabs/DeliveriesTab';
 
 export default function App() {
-  const { recipes } = useAppData();
-
   return (
     <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <ChefHat size={32} />
-      <h1 style={{ color: '#3d59ab' }}>PrepTab Import Test!</h1>
+      <h1 style={{ color: '#3d59ab' }}>DeliveriesTab Import Test!</h1>
       <p>Import successful</p>
     </div>
   );
