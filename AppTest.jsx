b576d071d1f521c64c46c9cb7b01bf46e4e41bf4
Paula McDade <paula@mac.mynetworksettings.com>
2026-01-25 19:49:23 -0500

    Test useAppData hook

--- AppTest.jsx
+++ AppTest.jsx
@@ -1,14 +1,18 @@
-// Testing lucide + constants
+// Testing lucide + constants + useAppData hook
 import React from 'react';
 import { ChefHat } from 'lucide-react';
 import { DEFAULT_NEW_CLIENT, TABS } from './constants';
+import { useAppData } from './hooks/useAppData';
 
 export default function App() {
+  const { clients, recipes } = useAppData();
+
   return (
     <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <ChefHat size={32} />
-      <h1 style={{ color: '#3d59ab' }}>Constants Test Works!</h1>
-      <p>Tabs: {TABS.length}</p>
+      <h1 style={{ color: '#3d59ab' }}>useAppData Test Works!</h1>
+      <p>Clients: {clients?.length || 0}</p>
+      <p>Recipes: {Object.keys(recipes || {}).length}</p>
     </div>
   );
 }
