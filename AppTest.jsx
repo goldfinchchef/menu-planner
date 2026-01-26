7c747faaa8afed5662bb17f5a4cb508bee662c31
Paula McDade <paula@mac.mynetworksettings.com>
2026-01-25 20:03:41 -0500

    Test ALL App.jsx imports

--- AppTest.jsx
+++ AppTest.jsx
@@ -1,7 +1,11 @@
-// Testing all 4 tabs + utils
-import React from 'react';
-import { ChefHat } from 'lucide-react';
+// Testing ALL App.jsx imports
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ChefHat, Settings } from 'lucide-react';
 import Papa from 'papaparse';
+import Tabs from './components/Tabs';
+import WorkflowStatus from './components/WorkflowStatus';
+import WeekSelector from './components/WeekSelector';
 import { useAppData } from './hooks/useAppData';
 import RecipesTab from './tabs/RecipesTab';
 import KDSTab from './tabs/KDSTab';
@@ -18,13 +22,15 @@ import {
   parseRecipesCSV,
   downloadCSV
 } from './utils';
+import { DEFAULT_NEW_CLIENT, DEFAULT_NEW_RECIPE, DEFAULT_NEW_MENU_ITEM, DEFAULT_NEW_INGREDIENT } from './constants';
 
 export default function App() {
   return (
     <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <ChefHat size={32} />
-      <h1 style={{ color: '#3d59ab' }}>All Tabs + Utils Test!</h1>
+      <h1 style={{ color: '#3d59ab' }}>ALL Imports Test!</h1>
       <p>Week: {getWeekId()}</p>
+      <p>All imports successful!</p>
     </div>
   );
 }
