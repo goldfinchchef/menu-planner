f5dbb83343cd425fcacd79394ab5474b2bbe4d0d
Paula McDade <paula@mac.mynetworksettings.com>
2026-01-25 19:50:47 -0500

    Test KDSTab

--- AppTest.jsx
+++ AppTest.jsx
@@ -1,8 +1,8 @@
-// Testing lucide + constants + useAppData hook
+// Testing with KDSTab
 import React from 'react';
 import { ChefHat } from 'lucide-react';
-import { DEFAULT_NEW_CLIENT, TABS } from './constants';
 import { useAppData } from './hooks/useAppData';
+import KDSTab from './tabs/KDSTab';
 
 export default function App() {
   const { clients, recipes } = useAppData();
@@ -10,9 +10,9 @@ export default function App() {
   return (
     <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
       <ChefHat size={32} />
-      <h1 style={{ color: '#3d59ab' }}>useAppData Test Works!</h1>
+      <h1 style={{ color: '#3d59ab' }}>KDSTab Test Works!</h1>
       <p>Clients: {clients?.length || 0}</p>
-      <p>Recipes: {Object.keys(recipes || {}).length}</p>
+      <KDSTab kdsView={{ monTue: {}, thursday: {} }} />
     </div>
   );
 }
